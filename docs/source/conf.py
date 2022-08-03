59b3030b4b7b3f309e12d46891edaac8e2165ece
aniskhan25 <aniskhan25@users.noreply.github.com>
2022-08-03 20:01:10 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'HMSC'
+copyright = '2022, JYU'
+author = 'JYU'
 
 release = '0.1'
 version = '0.1.0'
